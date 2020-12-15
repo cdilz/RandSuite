bf25260a14ea5d3e131b575f00b801b2df353abc
cdilz <49366094+cdilz@users.noreply.github.com>
2020-12-15 17:18:37 -0500

    Documentation for Deck class.

--- src/deck/base/deck.js
+++ src/deck/base/deck.js
@@ -1,25 +1,41 @@
 'use strict'
 
+/**
+ * Class representing a deck of cards.
+ * 
+ * To be used as a superclass only
+ * 
+ * @param {Generator} generator - Random number generator to use with the deck.
+ */
 class Deck
 {
-	constructor(randSuite)
+	constructor(generator)
 	{
 		this.freshDeck = []
 		this.deck = []
-		this.generator = randSuite
+		this.generator = generator
 	}
 
+	/**
+	 * Replaces the deck with a fresh one as if the class was just generated.
+	 */
 	reset()
 	{
 		this.deck = this.freshDeck
 	}
 
+	/**
+	 * Shuffles the entire deck, including those already drawn.
+	 */
 	shuffleAll()
 	{
 		this.reset()
 		this.shuffle()
 	}
 
+	/**
+	 * Shuffles whatever cards remain in the deck.
+	 */
 	shuffle()
 	{
 		let newCards = []
@@ -32,11 +48,21 @@ class Deck
 		this.deck = newCards
 	}
 
+	/**
+	 * Adds a card to the deck.
+	 * 
+	 * @param {Card} card 
+	 */
 	addCard(card)
 	{
 		this.deck.push(card)
 	}
 
+	/**
+	 * Removes a card from the deck.
+	 * 
+	 * @param {Card} card 
+	 */
 	removeCard(card)
 	{
 		for(let i = 0; i < this.deck.length; i++)
@@ -49,6 +75,11 @@ class Deck
 		}
 	}
 
+	/**
+	 * Draws a random card from the deck.
+	 * 
+	 * @returns {Card} - The card that was drawn.
+	 */
 	drawRandom()
 	{
 		let card = undefined
@@ -61,11 +92,23 @@ class Deck
 		return card
 	}
 
+	/**
+	 * Draws a card from the top of the deck.
+	 * 
+	 * @returns {Card} - The card that was drawn.
+	 */
 	draw()
 	{
 		return this.deck.pop()
 	}
 
+	/**
+	 * Reveals a card at a specific position in the deck.
+	 * 
+	 * @param {Number} index - The position in the deck.
+	 * 
+	 * @returns {Card} - The card at that position in the deck.
+	 */
 	inspect(index)
 	{
 		return this.deck[index]
