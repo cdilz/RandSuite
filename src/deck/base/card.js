b1fc04e0f4b1e1ec58d9e770dd3f7ed3e9618549
cdilz <49366094+cdilz@users.noreply.github.com>
2020-12-15 17:25:47 -0500

    Documentation for the Card class.

--- src/deck/base/card.js
+++ src/deck/base/card.js
@@ -10,6 +10,15 @@
 	Must be either a number or array of numbers (then trump cards can be [1,2,3,...] and Aces can be [1,11]) - Note this is dependant on game so not sure on it, might reduce names to just single characters instead and allow the game to figure it out
 
 */
+/**
+ * Class representing a playing card.
+ * 
+ * @param {String} color - The color of the card suit, usually black and red in a standard deck.
+ * @param {String} rank - The rank of the card, usually Ace, 2, 3, etc in a standard deck.
+ * @param {String} suit - The suit of a card, usually clubs, spades, hearts, and diamonds in a standard deck.
+ * @param {Boolean} faceUp - True if face up, false if it isn't.
+ * @param {Number} orientation - Degrees turned the card is. 0 is normal, 180 is upside down.
+ */
 class Card
 {
 	constructor(color, rank, suit, faceUp, orientation)
@@ -32,6 +41,13 @@ class Card
 		}
 	}
 
+	/**
+	 * Checks if the input card is equal to this card.
+	 * 
+	 * @param {Card} card - Card to compare to
+	 * 
+	 * @return {Boolean}
+	 */
 	equals(card)
 	{
 		let color = card.color == this.color
@@ -41,6 +57,11 @@ class Card
 		return suit && rank && color
 	}
 
+	/**
+	 * Outputs the card as a string.
+	 * 
+	 * @return {String} - String representation of this card as color rank of suit
+	 */
 	toString()
 	{
 		return this.color + ' ' + this.rank + ' of ' + this.suit 
