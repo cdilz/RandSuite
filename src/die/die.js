281acc5443b8e7f865b760d4f3407b1f1529ce59
cdilz <49366094+cdilz@users.noreply.github.com>
2020-12-15 15:32:11 -0500

    Documentation for the Die class.

--- src/die/die.js
+++ src/die/die.js
@@ -1,26 +1,36 @@
 'use strict'
 
-/*
-*	Class representing a normal game die of selectable size
-*/
+/**
+ * Class representing a numbered game die.
+ * 
+ * @param {Generator} generator - Random number generator used to roll the die.
+ * @param {Number} [sides=6] - The number of sides the die has, defaults to 6.
+ */
 class Die
 {
-	/*
-		Creates a die that can be rolled to get numbers
-		@param {RandSuite} randSuite - Used as the random number generator backend
-		@param {number} [sides=6] - The number of sides the die has, defaults to 6
-	*/
-	constructor(randSuite, sides = 6)
+	constructor(generator, sides = 6)
 	{
-		this.generator = randSuite
+		this.generator = generator
 		this.sides = sides
 	}
 
+	/**
+	 * Rolls the die.
+	 * 
+	 * @returns {BigInt} - A random number between 1 and the number of sides the die has, inclusive.
+	 */
 	roll()
 	{
 		return this.generator.numberBetween(1, this.sides)
 	}
 
+	/**
+	 * Rolls the die multiple times and adds them together.
+	 * 
+	 * @param {Number} times - Number of times to roll the die.
+	 * 
+	 * @returns {BigInt} - Number value of all rolls added together.
+	 */
 	rollAdd(times = 1)
 	{
 		if(times < 1)
@@ -37,6 +47,13 @@ class Die
 		return output
 	}
 
+	/**
+	 * Rolls the die multiple timnes and multiplies them together.
+	 * 
+	 * @param {Number} times - Number of times to roll the die.
+	 * 
+	 * @returns {BigInt} - Number of all rolls multiplied together.
+	 */
 	rollMultiply(times = 1)
 	{
 		if(times < 1)
@@ -53,6 +70,13 @@ class Die
 		return output
 	}
 
+	/**
+	 * Rolls the die multiple times and returns them as an array in roll order.
+	 * 
+	 * @param {Number} times - Number of times to roll the die.
+	 * 
+	 * @returns {BigInt[]} - Dice rolls in roll order.
+	 */
 	rollArray(times = 1)
 	{
 		if(times < 1)
