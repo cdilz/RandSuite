f1fb3162b954b348ed238893790fb22a1dbcddac
cdilz <49366094+cdilz@users.noreply.github.com>
2020-12-22 12:17:37 -0500

    Implemented a coin for flipping.

---
+++ src/coin/coin.js
@@ -0,0 +1,125 @@
+'use strict'
+
+/**
+ * Class representing a coin.
+ * 
+ * @param {Generator} generator - The random number generator to use with the coin.
+ * @param {Boolean} [outputType=Count.STANDARD] - How the user wants the results output.
+ */
+class Coin
+{
+	constructor(generator, outputType = Coin.outputTypes.STANDARD)
+	{
+		this.generator = generator
+		this.outputType = outputType
+	}
+
+	/**
+	 * Enum for the output types.
+	 * 
+	 * STANDARD: Heads, Tails
+	 * TECHNICAL: Obverse, Reverse
+	 * NUMERIC: 1, 0
+	 * BOOLEAN: True, False
+	 * 
+	 * @readonly
+	 * @enum {Number}
+	 */
+	static get outputTypes()
+	{
+		return {
+			STANDARD: 0,
+			TECHNICAL: 1,
+			NUMERIC: 2,
+			BOOLEAN: 3
+		}
+	}
+
+	/**
+	 * @type {String|Number|Boolean}
+	 */
+	get heads()
+	{
+		switch(this.outputType)
+		{
+			case Coin.outputTypes.STANDARD:
+				return 'Heads'
+			case Coin.outputTypes.TECHNICAL:
+				return 'Obverse'
+			case Coin.outputTypes.NUMERIC:
+				return 1
+			case Coin.outputTypes.BOOLEAN:
+				return true
+			default:
+				throw 'Output Type Does Not Exist'
+		}
+	}
+
+	/**
+	 * @type {String|Number|Boolean}
+	 */
+	get tails()
+	{
+		switch(this.outputType)
+		{
+			case Coin.outputTypes.STANDARD:
+				return 'Tails'
+			case Coin.outputTypes.TECHNICAL:
+				return 'Reverse'
+			case Coin.outputTypes.NUMERIC:
+				return 0
+			case Coin.outputTypes.BOOLEAN:
+				return false
+			default:
+				throw 'Output Type Does Not Exist'
+		}
+	}
+
+	/**
+	 * Flips a coun count number of times
+	 * 
+	 * @param {Number} count 
+	 * @param {Boolean} [forceArray=false]
+	 * 
+	 * @returns {String|String[]} - The results of the flip in order. Reduces to just the output if single flip, unless forceArray is true
+	 */
+	flip(count = 1, forceArray = false)
+	{
+		if(typeof count != typeof 0)
+		{
+			throw 'Count must be a Number.'
+		}
+		else if(count < 1)
+		{
+			throw 'Count must be a positive non-zero number'
+		}
+
+		let output = []
+
+		if(count == 1)
+		{
+			// Gets a random boolean, if it's heads, get the correct result for heads, else get the one for tails.
+			let result = this.generator.bool() ? this.heads : this.tails
+			if(forceArray)
+			{
+				output.push(result)
+			}
+			else
+			{
+				output = result
+			}
+		}
+		else
+		{
+			for(let i = 0; i < count; i++)
+			{
+				// Recurses once to flip a single coin.
+				output.push(this.flip())
+			}
+		}
+
+		return output
+	}
+}
+
+export default Coin
---
+++ test/coin/coin.js
@@ -0,0 +1,43 @@
+import CryptoRNG from '../../src/generator/crypto.js'
+import Coin from '../../src/coin/coin.js'
+
+
+
+let generator = new CryptoRNG({bits:1})
+let coinStan = new Coin(generator, Coin.outputTypes.STANDARD)
+let coinTech = new Coin(generator, Coin.outputTypes.TECHNICAL)
+let coinNum = new Coin(generator, Coin.outputTypes.NUMERIC)
+let coinBool = new Coin(generator, Coin.outputTypes.BOOLEAN)
+
+function test(coin)
+{
+	let output = {}
+	output[coin.heads] = 0
+	output[coin.tails] = 0
+	output.other = 0
+
+	let coins = coin.flip(10000)
+
+	for(let i = 0; i < coins.length; i++)
+	{
+		if(coins[i] == coin.heads)
+		{
+			output[coin.heads]++
+		}
+		else if(coins[i] == coin.tails)
+		{
+			output[coin.tails]++
+		}
+		else
+		{
+			output.other++
+		}
+	}
+
+	return output
+}
+
+console.log(test(coinStan))
+console.log(test(coinTech))
+console.log(test(coinNum))
+console.log(test(coinBool))
